f04d2b6a18adf44672c568e2bdfa404a46b3b27f
Harry Dhillon <hdhillon478@gmail.com>
2022-10-10 21:38:43 +0100

    Create useHttp custom hook, to make Http requests

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,5 +1,47 @@
 import { useState } from "react";
 
-const useHttp = () => {};
+/*
+ useHttp custom hook
+ Used to make Http requests, GET or POST
+ Hook, takes two parameters, 
+ 1. Object with {url, method, headers, body}
+ 2. Function, to handle the data returned from the request.
+ ------
+ Returns, 
+ error state, loading state and sendRequest function.
+*/
+
+const useHttp = (requestConfig, dataHandler) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendHttpRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      dataHandler(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendHttpRequest: sendHttpRequest,
+  };
+};
 
 export default useHttp;
