d3936552eeb1a6cc5e3e439e4b414cb77a105a2c
Harry Dhillon <hdhillon478@gmail.com>
2022-10-11 21:07:03 +0100

    Add useHttp custom hook call to app.js

--- src/App.js
+++ src/App.js
@@ -2,38 +2,30 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/useHttp";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-customhooks-87d57-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+  // Extract data objects from Firabase realtime db
+  // and push into array and update state
+  const transformData = (data) => {
+    const loadedTasks = [];
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
     }
-    setIsLoading(false);
+    setTasks(loadedTasks);
   };
+
+  // call custom hook
+  const httpData = useHttp(
+    {
+      url: "https://react-customhooks-87d57-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+    },
+    transformData
+  );
+
+  const { error, isLoading, sendHttpRequest: fetchTasks } = httpData;
 
   useEffect(() => {
     fetchTasks();
